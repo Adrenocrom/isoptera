a659e236e27274fc9de7032a57781873663ff56e
Josef Schulz <j.schulz@alaun.de>
2019-07-18 10:18:32 +0200

    update

---
+++ www/sw.js
@@ -0,0 +1,43 @@
+var version = "0.0.35";
+var cacheName = "isoptera";
+
+self.addEventListener('install', function(event) {
+	console.log("The service worker is being installed.");
+	event.waitUntil(
+		caches.open(cacheName).then(function(cache) {
+			return cache.addAll([
+				"/",
+            	"/css/style.css",
+				"/js/a3Utils.js",
+				"/js/a3Gl.js",
+				"/js/app.js",
+				"/js/lib/gl-matrix-min.js",
+            	"/img/logo.png",
+            	"/img/kiste.png",
+            	"/img/sprite.jpg",
+            	"/img/sprite2.png",
+				"/img/reload.png",
+            	"/manifest.json",
+            	"/favicon.ico"
+			]);
+	}));
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.open(cacheName).then(function(cache) {
+			return cache.match(event.request)
+				.then(function(response) {
+					if(response)
+						return response;
+					return fetch(event.request);
+			});
+		})
+	);
+});
+
+self.addEventListener("message", function(event) {
+	if(event.data.action === "skipWaiting") {
+		self.skipWaiting();
+	}
+});
